64ed598de06f6252d723fee65fea492b3e27c8d2
arifai1 <arifaiwu@gmail.com>
2022-05-30 21:27:35 -0300

    Updated script
    Jump, go left, go right with keyboard

--- Assets/SphereMovement.cs
+++ Assets/SphereMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SphereMovement : MonoBehaviour
 {
+    public GameObject mysphere;
+    float movementspeed = 0.1f;
+    float jumping = 0.2f;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,18 @@ public class SphereMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mysphere.transform.position += new Vector3(-movementspeed, 0, 0);
+        if(Input.GetKey(KeyCode.A))
+        {
+            mysphere.transform.position += new Vector3(0, 0, -movementspeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            mysphere.transform.position += new Vector3(0, 0, movementspeed);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            mysphere.transform.position += new Vector3(0, jumping, 0);
+        }
     }
 }
