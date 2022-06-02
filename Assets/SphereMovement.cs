f15e302e5759cb21b89e1158503f0f892b231a74
ari <arifaiwu@gmail.com>
2022-06-02 14:07:44 -0300

    Updated Time

--- Assets/SphereMovement.cs
+++ Assets/SphereMovement.cs
@@ -5,25 +5,31 @@ using UnityEngine.UI;
 
 public class SphereMovement : MonoBehaviour
 {
-    //public GameObject mysphere;
+    GameObject mysphere;
     public float movementspeed = 0.1f;
     public float jumping = 0.15f;
     Vector3 startPos;
     public GameObject perdiste;
     public GameObject ganaste;
-
+    int counter = 0;     
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position;
         perdiste.SetActive(!perdiste.activeInHierarchy);
         ganaste.SetActive(!ganaste.activeInHierarchy);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        int tiempo = Mathf.FloorToInt(Time.time);
         transform.position += new Vector3(-movementspeed, 0, 0) * Time.deltaTime;
+        while (tiempo > counter)
+        {
+            movementspeed = movementspeed + 0.1f;
+        }
         if(Input.GetKey(KeyCode.A))
         {
             transform.position += new Vector3(0, 0, -movementspeed) * Time.deltaTime;
@@ -35,6 +41,11 @@ public class SphereMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += new Vector3(0, jumping, 0) * Time.deltaTime;
+            while (counter <=5)
+            {
+                Instantiate(mysphere);
+                counter++;
+            }
         }
     }
 
